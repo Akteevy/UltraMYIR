490485c05e0b41001d46c7fc54a997ac113e797b
Akteevy <58146041+Akteevy@users.noreply.github.com>
2020-04-25 23:24:42 +0100

    first commit

---
+++ 05 NVMe SSD on PCIe/Vivado/sources/ps_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+//Date        : Sat Apr 25 11:45:43 2020
+//Host        : ubu18xilinx running 64-bit Ubuntu 18.04.4 LTS
+//Command     : generate_target ps_wrapper.bd
+//Design      : ps_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module ps_wrapper
+   ();
+
+
+  ps ps_i
+       ();
+endmodule
